76b06128a21baf95d2034f46e5b7717b61e9d941
TedCAI <ted.xmdgj@gmail.com>
2015-10-07 18:51:32 +0100

    update

---
+++ functions.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+
+bool sort_by_name (Station* const (&p1), Station* const (&p2));
+void find_index(const vector<Station *> & station_list, Station * &station, int index);
+Station * station_index_to_name(const vector<Station *> & station_list, int index);
+vector<string> next_stations(const vector<Station *> & station_list, const vector<int> & index);
+void print_station_info(const vector<Station *> & station_list, Station * station, int mode = 0);
+void print_all(const vector<Station *> & print_list, const vector<Station *> & original_list, int mode = 0);
+void match_name(const string & name, Station* const &station, vector<Station *> & result_station_list);
+void match_all(const string & name, vector<Station *> & result_station_list, const vector<Station *> & station_list);
+vector <Station *> station_search_by_name(const string & name, const vector<Station *> & station_list);
+void search_by_keyword(const vector<Station *> & search_list, const vector<Station *> & original_list);
+void station_at(const int & line, Station* const &station, vector<Station *> & result_station_list);
+void all_station_at(const int & line, vector<Station *> & match_station_list, const vector<Station *> & search_list);
+vector<Station *> station_search_by_line(const int & line, const vector<Station *> & search_list);
+void search_by_line(const vector<Station *> & search_list, const vector<Station *> & original_list);
+void mode_selection(const vector<Station *> & search_list, const vector<Station *> & original_list);
--- main.cpp
+++ main.cpp
@@ -5,51 +5,20 @@
 #include <vector>
 #include <stdio.h>
 #include "fileIO.h"
-//#include "station.h"
+#include <algorithm>
+#include "functions.h"
 
 using namespace std;
-
-vector<string> next_stations(const vector<Station *> & station_list, const vector<int> & index){
-	vector<string> stations;
-	vector<int> indice = index;
-	for(vector<int>::const_iterator it = indice.begin();it != indice.end();++it){
-		Station * station = station_list[*it];
-		string name = station->get_station_name();
-		stations.push_back(name);
-	}
-	return stations;
-}
-
-void print_station_info(const vector<Station *> & station_list, Station * station){
-	vector<int> next_station;
-	vector<string> lines, next_station_str;
-	lines = station->line_names();
-//	next_station = station->get_station_next_station();
-	next_station_str = next_stations(station_list, station->get_station_next_station());
-
-        cout<<"======================================="<<endl;
-        cout<<"Index: "<<station->get_station_index()<<endl;
-        cout<<"Station Name: "<<station->get_station_name()<<endl;
-        cout<<"Located At: ";
-	
-        for(vector<string>::iterator it = lines.begin();it != lines.end();++it)
-                cout<<*it<<" ";
-//              cout<<*it<<" ";
-        cout<<endl;
-        cout<<"Next Station: ";
-//        for(vector<int>::iterator it = next_station.begin();it != next_station.end();++it)
-	for(vector<string>::iterator it = next_station_str.begin();it != next_station_str.end();++it)
-                cout<<*it<<" ";
-        cout<<endl;
-}
 
 int main(){
 	FileIO * file = new FileIO("tube.txt");
 	vector<Station *> station_list = file->read_file_to_station_list();
-	for(vector<Station *>::iterator it = station_list.begin();it != station_list.end();++it)
-		print_station_info(station_list, *it);
+	vector<Station *> sorted_list = file->read_file_to_station_list();
+	std::sort(sorted_list.begin(), sorted_list.end(), sort_by_name);
+	print_all(sorted_list, station_list);
 
-//	station_list[0]->print_station_info();
-	
+//	search_by_keyword(sorted_list, station_list);
+	mode_selection(sorted_list, station_list);
+
 	return 0;
 }
