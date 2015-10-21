514e33a9ef3e11be424eedad2b144362a6c8f702
TedCAI <ted.xmdgj@gmail.com>
2015-10-21 18:46:28 +0100

    Add balance function (with bug)

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -27,46 +27,27 @@ public:
 	node(const T &d, const int &h = 0, node<T> * const &f = NULL, node<T> * const &l = NULL, node<T> * const &r = NULL){
 		data = d;
 		height = h;
-		cout<<"cons"<<endl;
-		if(l)
-			lhs = new node<T>(l->data, l->height, l->father, l->lhs, l->rhs);
-		else
-			lhs = NULL;
-		cout<<"cons lhs"<<endl;
-		if(r)
-			rhs = new node<T>(r->data, r->height, r->father, r->lhs, r->rhs);
-		else
-			rhs = NULL;
-		cout<<"cons rhs"<<endl;
-		if(f)
-			father = f;
-//			father = new node<T>(f->data, f->height, f->father, f->lhs, f->rhs);
-		else
-			father = NULL;
-		cout<<"cons father"<<endl;
+//		cout<<"cons"<<endl;
+		lhs = l;
+//		cout<<"cons lhs"<<endl;
+		rhs = r;
+//		cout<<"cons rhs"<<endl;
+		father = f;
+//		cout<<"cons father"<<endl;
 	}
 
 	node( const node<T> & _node){
 		if(&_node){
-			cout<<"copy"<<endl;
+//			cout<<"copy"<<endl;
 			data = _node.data;
-			cout<<"copy data "<<data<<endl;
+//			cout<<"copy data "<<data<<endl;
 			height = _node.height;
-			if(!(_node.lhs))
-				lhs = NULL;
-			else
-				lhs = new node<T>((_node.lhs)->data, (_node.lhs)->height, (_node.lhs)->father, (_node.lhs)->lhs, (_node.lhs)->rhs);
-			cout<<"copy lhs"<<endl;
-			if(!(_node.rhs))
-				rhs = NULL;
-			else
-				rhs = new node<T>((_node.rhs)->data, (_node.rhs)->height, (_node.rhs)->father, (_node.rhs)->lhs, (_node.rhs)->rhs);
-			cout<<"copy rhs"<<endl;
-			if(!(_node.father))
-				father = NULL;
-			else
-				father = new node<T>((_node.father)->data, (_node.father)->height, (_node.father)->father, (_node.father)->lhs, (_node.father)->rhs);
-			cout<<"copy father"<<endl;
+			lhs = _node.lhs;
+//			cout<<"copy lhs"<<endl;
+			rhs = _node.rhs;
+//			cout<<"copy rhs"<<endl;
+			father = _node.father;
+//			cout<<"copy father"<<endl;
 		}
 		else{
 			data = T();
@@ -76,30 +57,37 @@ public:
 	}
 
 	node<T> & operator=(const node<T> & _node){
-		cout<<"operator"<<endl;
+//		cout<<"operator"<<endl;
 		if(this != &(_node)){
 			data = _node.data;
-			cout<<"data"<<endl;
+//			cout<<"data"<<endl;
 			height = _node.height;
-			cout<<"height"<<endl;
-			if(!(_node.lhs))
-				lhs = NULL;
-			else
-				lhs = new node<T>(*(_node.lhs));
-			cout<<"lhs"<<endl;
-			if(!(_node.rhs))
-				rhs = NULL;
-			else
-				rhs = new node<T>(*(_node.rhs));
-			cout<<"rhs"<<endl;
-			if(!(_node.father))
-				father = NULL;
-			else
-				father = new node<T>(*(_node.father));
-			cout<<"father"<<endl;
+//			cout<<"height"<<endl;
+			lhs = _node.lhs;
+//			cout<<"lhs"<<endl;
+			rhs = _node.rhs;
+//			cout<<"rhs"<<endl;
+			father = _node.father;
+//			cout<<"father"<<endl;
 		}
-		cout<<"operator done"<<endl;
+//		cout<<"operator done"<<endl;
 		return *this;
+	}
+
+	~node<T>(){
+		if(this){
+//			cout<<"delete node "<<data<<" "<<lhs<<" "<<rhs<<endl;
+			if(lhs){
+//				cout<<"lhs "<<endl;
+//				cout<<"delete lhs "<<lhs->data<<endl;
+				delete lhs;
+			}
+			if(rhs){
+//				cout<<"delete rhs "<<rhs->data<<endl;
+				delete rhs;
+			}
+			data = -1;
+		}
 	}
 };
 
@@ -115,7 +103,7 @@ public:
 	}
 	
 	binary_tree(node<T> * new_root){
-		root = new node<T>(*new_root);
+		root = new_root;
 		tree_height = 0;
 		count = 0;
 	}
@@ -142,14 +130,15 @@ public:
 			return parent_node;
 		}
 		else{
-			cout<<"parent_node father "<<parent_node->father<<endl;
+//			cout<<"parent_node father "<<parent_node->father<<endl;
 			_hot = parent_node;
-			cout<<"_hot father "<<_hot->father<<endl;
+//			cout<<"_hot father "<<_hot->father<<endl;
 			return search_tree(value, (value < parent_node->data) ? parent_node->lhs : parent_node->rhs, _hot);
 		}
 	}
 
 	virtual bool insert(const T & value){
+//		cout<<"BST insert"<<endl;
 		node<T> *_hot = new node<T>();
 		node<T> *returned_node = search_tree(value, root, _hot);
 		if(returned_node)
@@ -158,7 +147,7 @@ public:
 			returned_node = new node<T>();
 			returned_node->data = value;
 			returned_node->height = _hot->height + 1;
-			cout<<_hot->father<<endl;
+//			cout<<_hot->father<<endl;
 //			returned_node->father = new node<T>(*_hot);
 			if((_hot->data) < value){
 				_hot->rhs = returned_node;
@@ -193,15 +182,15 @@ public:
 			min = find_min();
 		}
 		else{
-			cout<<"find 1"<<endl;
+//			cout<<"find 1"<<endl;
 //			node<T> *parent = start;
 //			cout<<"find 2"<<endl;
 			while(start->lhs){
-				cout<<start->data<<" "<<start->height<<" "<<!(start->lhs)<<endl;
+//				cout<<start->data<<" "<<start->height<<" "<<!(start->lhs)<<endl;
 				start = start->lhs;
-				cout<<start->data<<endl;
+//				cout<<start->data<<endl;
 			}
-			cout<<"find 3"<<endl;
+//			cout<<"find 3"<<endl;
 			min = *start;
 		}
 	}
@@ -257,6 +246,11 @@ public:
 		find_depth_in(parent, depth, init_depth);
 		return depth;
 	}
+
+	virtual ~binary_tree<T>(){
+		cout<<"delete binary tree"<<endl;
+		delete root; 
+	}
 };
 
 template<typename T>
@@ -267,15 +261,39 @@ public:
 	AVL(node<T> new_root):binary_tree<T>(new_root){};
 
 	int balance_factor(node<T> * parent){
-		return abs(this->find_depth(parent->lhs, 0) - this->find_depth(parent->rhs, 0));
+		return (this->find_depth(parent->lhs, 0) - this->find_depth(parent->rhs, 0));
 	}
 	
 	bool is_balanced(node<T> * parent){
-		return (balance_factor(parent) <= 1);
+		int factor = balance_factor(parent);
+		return (factor <= 1 && factor >= -1);
 	}
 
-//	bool insert(const T &value){
-/*		node<T> *_hot = new node<T>();
+	void rebalance(node<T> * parent){
+		if(parent)
+			if(!is_balanced(parent)){
+				if(balance_factor(parent) < -1){
+//					node<T> * tmp_father = parent->father;
+					parent->father->rhs = parent->lhs;
+					parent->lhs->father = parent->father;
+//					parent->father = parent->father->father;
+					parent->lhs = parent->father;
+					parent->father = parent->father->father;
+					parent->lhs->father = parent;
+				}
+				else{
+					parent->father->lhs = parent->rhs;
+					parent->rhs->father = parent->father;
+					parent->rhs = parent->father;
+					parent->father = parent->father->father;
+					parent->rhs->father = parent;
+				}
+			}
+	}
+
+	virtual bool insert(const T &value){
+//		cout<<"AVL insert"<<endl;
+		node<T> *_hot = new node<T>();
                 node<T> *returned_node = this->search_tree(value, this->root, _hot);
                 if(returned_node)
                         return false;
@@ -293,9 +311,16 @@ public:
                         this->update_tree_height(returned_node->height);
                         this->count++;
 
-                        
+			rebalance(_hot);
+
+                 	return true;       
                 }
-*///	}
+	}
+
+	virtual ~AVL<T>(){
+		cout<<"delete AVL tree"<<endl;
+//		delete[] this->root;
+	}
 };
 
 int main(){
@@ -317,20 +342,20 @@ int main(){
 
 	for(vector<Station *>::iterator it = sorted_list.begin() + 1;it != sorted_list.end();++it){
 		bool b_return = tree->insert((*it)->get_station_index());
-		cout<<(*it)->get_station_index()<<"  "<<b_return<<endl;
+//		cout<<(*it)->get_station_index()<<"  "<<b_return<<endl;
 	}
 
 //	tree->traverse_inorder(tree->root);
 	cout<<"Tree height is "<<tree->tree_height<<endl;
-	node<int> *returned_node = tree->search(12);
+	node<int> *returned_node = tree->search(0);
 	cout<<returned_node->data<<" "<<returned_node->height<<endl;
 	node<int> *rhs = tree->root;
-	cout<<rhs->data<<" "<<rhs->height<<endl;
+//	cout<<rhs->data<<" "<<rhs->height<<endl;
 	rhs = rhs->rhs;
 	node<int> min, max;
-	cout<<"d 1"<<endl;
+//	cout<<"d 1"<<endl;
 	tree->find_min_from(rhs, min);
-	cout<<"d 2"<<endl;
+//	cout<<"d 2"<<endl;
 	tree->find_max_from((tree->root)->lhs, max);
 	cout<<"min in right branch is "<<min.data<<" "<<min.height<<endl;
 	cout<<"max in left branch is "<<max.data<<" "<<max.height<<endl;
@@ -341,5 +366,8 @@ int main(){
 	cout<<"balance factor is "<<tree->balance_factor(tree->root)<<endl;
 	cout<<"tree is balanced? "<<tree->is_balanced(tree->root)<<endl;
 
+	cout<<"tree root "<<(tree->root)->data<<endl;
+//	delete[] tree->root;
+	delete tree;
 	return 0;
 }
